659152315f3ddc01c32ee3548dce5cf71157c531
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-08 18:19:37 -0700

    Added error handling middleware

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -46,6 +46,10 @@ router.post('/:id/posts', validateUserId, validatePost, (request, response) => {
   // and another middleware to check that the request body is valid
 });
 
+router.use((error, request, response, next) => {
+  response.status(error.status || 500).json({ message: 'An internal server error occurred within the users router.' });
+})
+
 // do not forget to export the router
 
 module.exports = router;
